f90b1eb5421080eeb52a9d8791819c115ca21933
tanishqsujan <tanishq.kothamasu@gmail.com>
2025-12-06 16:44:20 +0400

    Sudoku

--- sudoku.py
+++ sudoku.py
@@ -34,8 +34,10 @@ def solveSudoku(grid, row, col):
     if grid[row][col] > 0:
         return solveSudoku(grid, row, col + 1)
     for num in range(1, N + 1, 1):
-        if solveSudoku(grid, row, col + 1):
-            return True 
+        if isSafe(grid, row, col, num):
+            grid[row][col] = num
+            if solveSudoku(grid, row, col + 1):
+                return True 
         
         grid[row][col] = 0
     return False
